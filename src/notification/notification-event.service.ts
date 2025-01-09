2dd3bf0fd0bf85ab04aa5e3d2c8f2ed29eafb3c1
Alexander <sashakypinko@gmail.com>
2025-01-09 14:52:02 +0100

    feat: Improve the notifications module

--- src/notification/notification-event.service.ts
+++ src/notification/notification-event.service.ts
@@ -4,12 +4,16 @@ import {INotification} from './notification.schema';
 
 @Injectable()
 class NotificationEventService extends SocketEventService<INotification> {
+  constructor() {
+    super('notification');
+  }
+  
   created(userConnections: UserConnectionsType, notification: INotification): void {
-    this.emit('created', userConnections.get(notification.receiver.toString()), notification);
+    this.emit('created', userConnections.get(notification.receiver._id.toString()), notification);
   }
 
   deleted(userConnections: UserConnectionsType, notification: INotification): void {
-    this.emit('deleted', userConnections.get(notification.receiver.toString()), notification);
+    this.emit('deleted', userConnections.get(notification.receiver._id.toString()), notification);
   }
 }
 
--- src/notification/notification.service.ts
+++ src/notification/notification.service.ts
@@ -4,6 +4,7 @@ import {CreateNotificationDto} from './dto/create-notification.dto';
 import EntityService from '../core/services/entity.service';
 import NotificationEventService from './notification-event.service';
 import TYPES from '../types';
+import {FilterQuery} from 'mongoose';
 
 @Injectable()
 class NotificationService extends EntityService<INotification> {
@@ -17,13 +18,9 @@ class NotificationService extends EntityService<INotification> {
   }
 
   async create(data: CreateNotificationDto, userConnections: UserConnectionsType): Promise<INotification> {
-    const notification = new this.model({
-      ...data,
-      
-    });
+    const notification = new this.model(data);
     await notification.save();
-    
-    this.notificationEventService.created(userConnections, notification);
+    this.notificationEventService.created(userConnections, await notification.populate(['sender', 'receiver']));
     
     return notification;
   }
@@ -33,7 +30,16 @@ class NotificationService extends EntityService<INotification> {
     if (!notification) throw new BadRequestException('Notification not found');
 
     await this.model.deleteOne({ _id: id });
+    this.notificationEventService.deleted(userConnections, notification);
+    
+    return notification;
+  }
 
+  async deleteByParams(params: FilterQuery<INotification>, userConnections: UserConnectionsType): Promise<INotification | null> {
+    const notification: INotification | null = await this.model.findOne(params);
+    if (!notification) throw new BadRequestException('Notification not found');
+
+    await notification.deleteOne();
     this.notificationEventService.deleted(userConnections, notification);
     
     return notification;
